37115e0ba2fca19fc9d04e3e544258e810d73207
Wrood Mahmoud <wroodmahmood409@gmail.com>
2022-11-22 14:50:42 +0200

    auth_design

--- lib/models/goal.dart
+++
@@ -1,8 +0,0 @@
-class Goal {
-  final int id;
-  final String title;
-  final String description;
-  final String image;
-
-  Goal({required this.id, required this.title,required this.description,required this.image});
-}
